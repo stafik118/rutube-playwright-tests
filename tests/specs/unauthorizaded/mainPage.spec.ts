6608f30850ce5af1c5985ff8bf6e4fb674fbf595
stafik118 <staf118@mail.ru>
2025-08-15 20:57:05 +0300

    add main page fixtures

--- tests/specs/unauthorizaded/mainPage.spec.ts
+++ tests/specs/unauthorizaded/mainPage.spec.ts
@@ -1,25 +1,13 @@
-import test from '@playwright/test';
+import { test, expect } from '../../fixtures/fixtures';
 import { MainPage } from '../../pages/mainPage';
 
-test('Открытие главной страницы', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-});
-test('Проверка доступности элементов хэдера', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.closeCookiesAlert();
+test('Открытие главной страницы', async ({ mainPage }) => {});
+test('Проверка доступности элементов хэдера', async ({ mainPage }) => {
   await mainPage.headerHasCorrectAriaSnapshot();
 });
-test('Проверка доступности элементов табов категории', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.closeCookiesAlert();
+test('Проверка доступности элементов табов категории', async ({ mainPage }) => {
   await mainPage.categoriesTabsAriaSnapshot();
 });
-test('Проверка доступности элементов бокового меню', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.closeCookiesAlert();
+test('Проверка доступности элементов бокового меню', async ({ mainPage }) => {
   await mainPage.menuHasCorrectAriaSnapshot();
 });
